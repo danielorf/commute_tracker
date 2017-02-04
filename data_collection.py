356e638ed994416839758a7d5d71073f8959e2b6
Daniel Orf <danielorf@Daniels-MacBook-Pro.local>
2017-02-03 22:16:45 -0800

    added mysql connection

--- data_collection.py
+++ data_collection.py
@@ -1,9 +1,8 @@
 import googlemaps
 import datetime
-from googlemaps.directions import directions
 import time
 
-#import pymysql
+import pymysql
 
 
 geocode_api_key = 'AIzaSyDGoq8IaQO0CBXlXF7pp20S6_kkon2iWBE'
@@ -16,47 +15,43 @@ home = '47.725065, -122.359203'
 red = '47.689102, -122.149295'
 
 
-'''
-#dir_result = gmaps.directions("4413 Chennault Beach Rd, Mukilteo, WA", "11700 Mukilteo Speedway, Mukilteo, WA")
-
-dir_result = gmaps.directions(home, there, departure_time=datetime.datetime.now())
-dir_result_rev = gmaps.directions(there, home, departure_time=datetime.datetime.now())
-
-print(dir_result[0]['legs'][0]['duration'])
-print(dir_result_rev[0]['legs'][0]['duration'])
-print()
-print(dir_result[0]['legs'][0]['duration_in_traffic'])
-print(dir_result_rev[0]['legs'][0]['duration_in_traffic'])
-'''
-
-
 #db = pymysql.connect(host='192.168.100.3', port=32769, user='admin', passwd='9ijznW2xjfcg', db='commute')
-# db = pymysql.connect(host='192.168.100.3', port=32776, user='admin', passwd='oYwU50bjQ4Et', db='commute')
-# cursor = db.cursor()
-# sql = """CREATE TABLE IF NOT EXISTS commute (
-#     id INT PRIMARY KEY AUTO_INCREMENT,
-#     date_time INT,
-#     time2red INT,
-#     time2home INT)"""
-#
-# cursor.execute(sql)
-# db.close()
+db = pymysql.connect(host='192.168.100.3', port=32776, user='admin', passwd='oYwU50bjQ4Et', db='commute')
+cursor = db.cursor()
+sql = """CREATE TABLE IF NOT EXISTS commute (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    year INT,
+    month INT,
+    day INT,
+    day_code INT,
+    hour INT,
+    minute INT,
+    time2red INT,
+    time2home INT)"""
+
+cursor.execute(sql)
+db.close()
 
 while True:
     #db = pymysql.connect(host='192.168.100.3', port=32769, user='admin', passwd='9ijznW2xjfcg', db='commute')
-    # db = pymysql.connect(host='192.168.100.3', port=32776, user='admin', passwd='oYwU50bjQ4Et', db='commute')
-    # cursor = db.cursor()
+    db = pymysql.connect(host='192.168.100.3', port=32776, user='admin', passwd='oYwU50bjQ4Et', db='commute')
+    cursor = db.cursor()
     try:
-        now = datetime.datetime.now()
+        now_object = datetime.datetime
+        now = now_object.now()
         dir2red = gmaps.directions(home, red, departure_time=now)
         dir2home = gmaps.directions(red, home, departure_time=now)
         time2red = dir2red[0]['legs'][0]['duration_in_traffic']
         time2home = dir2home[0]['legs'][0]['duration_in_traffic']
 
-        print(time.time())
-        print(now)
-        print(now.hour)
-        print(datetime.datetime.today().weekday())
+        year = int(now.year)
+        month = int(now.month)
+        day = int(now.day)
+        hour = int(now.hour)
+        minute = int(now.minute)
+        day_code = int(now_object.today().weekday())
+
+        print(year, ' ', month, ' ', day, ' ', hour, ' ', minute)
         print('Time to red: ', time2red)
         print('Time to home: ', time2home)
         print()
@@ -68,11 +63,10 @@ while True:
 
 
 
-    # sql2 = "INSERT INTO `commute` (`date_time`, `time2red`, `time2home`) VALUES (%s, %s, %s)"
-    # cursor.execute(sql2, (int(time.time()), int(time2red['value']), int(time2home['value'])))
-    # #cursor.execute(sql2, (1, 2, 3))
-    # db.commit()
-    # print("commited to db")
-    # db.close()
-    # print("closed db connection")
+    sql2 = "INSERT INTO `commute` (`year`, `month`, `day`, `day_code`, `hour`, `minute`, `time2red`, `time2home`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute(sql2, (year, month, day, day_code, hour, minute, int(time2red['value']), int(time2home['value'])))
+    db.commit()
+    print("commited to db")
+    db.close()
+    print("closed db connection")
     time.sleep(150)
