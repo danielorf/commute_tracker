eb5632060b569dfd61612008a5c1281ebe34c753
danielorf <danielorf@gmail.com>
2017-02-11 21:36:08 -0800

    Added test data to test remotely without SQL server

--- ct_server.py
+++ ct_server.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify, make_response, render_template
 from helper_functions import get_commute_data, get_commute_data_by_day
+from testing_data import test_date_list, test_time_to_red_list, test_time_to_home_list
 
 app = Flask(__name__)
+
+# Testing data switch
+use_testing_data = False
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -10,7 +15,13 @@ def not_found(error):
 
 @app.route('/commute/', methods=['GET'])
 def get_commute_plot():
-    date_list, min_to_red_list, min_to_home_list = get_commute_data(5000)
+    if use_testing_data:
+        date_list = test_date_list
+        min_to_red_list = test_time_to_red_list
+        min_to_home_list = test_time_to_home_list
+    else:
+        date_list, min_to_red_list, min_to_home_list = get_commute_data(5000)
+
     print(date_list)
     print(min_to_red_list)
     return render_template('commute.html',
@@ -22,7 +33,13 @@ def get_commute_plot():
 
 @app.route('/commute/<int:num>', methods=['GET'])
 def get_commute_plot_num(num):
-    date_list, min_to_red_list, min_to_home_list = get_commute_data(num)
+    if use_testing_data:
+        date_list = test_date_list
+        min_to_red_list = test_time_to_red_list
+        min_to_home_list = test_time_to_home_list
+    else:
+        date_list, min_to_red_list, min_to_home_list = get_commute_data(num)
+
     print(date_list)
     print(min_to_red_list)
     return render_template('commute.html',
@@ -32,13 +49,16 @@ def get_commute_plot_num(num):
                            plot_data_min2red=min_to_red_list,
                            plot_data_min2home=min_to_home_list)
 
-#date_list = ['02:34', '03:56', '05:06']
-#min_to_red_list = [1.00, 2.00, 3.00]
-#min_to_home_list = [2.00, 3.00, 5.00]
 
 @app.route('/commute_day/<int:day_code>', methods=['GET'])
 def get_commute_plot_by_day(day_code):
-    date_list, min_to_red_list, min_to_home_list = get_commute_data_by_day(5000,day_code)
+    if use_testing_data:
+        date_list = test_date_list
+        min_to_red_list = test_time_to_red_list
+        min_to_home_list = test_time_to_home_list
+    else:
+        date_list, min_to_red_list, min_to_home_list = get_commute_data_by_day(5000,day_code)
+
     print(date_list)
     print(min_to_red_list)
     return render_template('commute.html',
@@ -49,4 +69,6 @@ def get_commute_plot_by_day(day_code):
                            plot_data_min2home=min_to_home_list)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,
+            host='0.0.0.0'
+            )
