f579d5a77a4fa49034311b7f9b2cf6bbed2373de
danielorf <danielorf@gmail.com>
2017-02-21 20:40:47 -0800

    Added location selection and label fixes

--- ct_server.py
+++ ct_server.py
@@ -12,6 +12,7 @@ use_testing_data = False
 
 db_column_list_from_home = tokens_and_addresses.db_column_list_from_home
 db_column_list_to_home = tokens_and_addresses.db_column_list_to_home
+location = tokens_and_addresses.location_list[0]  # Default to first on list
 
 
 @app.errorhandler(404)
@@ -35,14 +36,16 @@ def get_default_commute_plot():
     print('Total time: ', time.time() - start_time)
 
     return render_template('commute.html',
-                           plot_to_red_name='time 2 downtown',
-                           plot_to_home_name='time 2 home',
+                           location=location,
+                           plot_from_home_name='time to '+location,
+                           plot_to_home_name='time to Home',
                            plot_data_date=downtown_commute.date_list,
-                           plot_data_min2red=downtown_commute.drive_time_from_home_list,
-                           plot_data_min2home=downtown_commute.drive_time_to_home_list)
+                           plot_data_from_home=downtown_commute.drive_time_from_home_list,
+                           plot_data_to_home=downtown_commute.drive_time_to_home_list)
 
 @app.route('/commute/<dest>', methods=['GET'])
 def get_commute_plot(dest):
+    dest = dest.lower()
     for column in db_column_list_from_home:
         if dest in column:
             from_home_db_column = column
@@ -62,14 +65,16 @@ def get_commute_plot(dest):
     print('Total time: ', time.time() - start_time)
 
     return render_template('commute.html',
-                           plot_to_red_name=from_home_commute_name,
-                           plot_to_home_name=to_home_commute_name,
+                           location=location,
+                           plot_from_home_name='time to '+location,
+                           plot_to_home_name='time to Home',
                            plot_data_date=commute.date_list,
-                           plot_data_min2red=commute.drive_time_from_home_list,
-                           plot_data_min2home=commute.drive_time_to_home_list)
+                           plot_data_from_home=commute.drive_time_from_home_list,
+                           plot_data_to_home=commute.drive_time_to_home_list)
 
 @app.route('/commute/<dest>/<int:num>', methods=['GET'])
 def get_commute_plot_num(dest, num):
+    dest = dest.lower()
     for column in db_column_list_from_home:
         if dest in column:
             from_home_db_column = column
@@ -89,15 +94,17 @@ def get_commute_plot_num(dest, num):
     print('Total time: ', time.time() - start_time)
 
     return render_template('commute.html',
-                           plot_to_red_name=from_home_commute_name,
-                           plot_to_home_name=to_home_commute_name,
+                           location=location,
+                           plot_from_home_name='time to '+location,
+                           plot_to_home_name='time to Home',
                            plot_data_date=commute.date_list,
-                           plot_data_min2red=commute.drive_time_from_home_list,
-                           plot_data_min2home=commute.drive_time_to_home_list)
+                           plot_data_from_home=commute.drive_time_from_home_list,
+                           plot_data_to_home=commute.drive_time_to_home_list)
 
 
 @app.route('/commute_day/<dest>/<int:day_code>', methods=['GET'])
 def get_commute_plot_by_day(dest, day_code):
+    dest = dest.lower()
     # if use_testing_data:
     #     date_list = test_date_list
     #     min_to_red_list = test_time_to_red_list
@@ -124,14 +131,22 @@ def get_commute_plot_by_day(dest, day_code):
     print('Total time: ', time.time() - start_time)
 
     return render_template('commute.html',
-                           plot_to_red_name=from_home_commute_name,
-                           plot_to_home_name=to_home_commute_name,
+                           location=location,
+                           plot_from_home_name='time to '+location,
+                           plot_to_home_name='time to Home',
                            plot_data_date=commute.date_list,
-                           plot_data_min2red=commute.drive_time_from_home_list,
-                           plot_data_min2home=commute.drive_time_to_home_list)
+                           plot_data_from_home=commute.drive_time_from_home_list,
+                           plot_data_to_home=commute.drive_time_to_home_list)
+
+@app.route('/change_location/<loc>', methods=['GET'])
+def change_location(loc):
+    global location
+    location = loc
+    return get_default_commute_plot()
 
 if __name__ == '__main__':
     app.run(debug=True,
-            host='0.0.0.0'
+            host='0.0.0.0',
+            port=8000
             )
 
