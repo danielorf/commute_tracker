bad036a9b5eddf6a1fc29865f0a9890b77e3fe27
Daniel Orf <danielorf@gmail.com>
2018-02-22 19:13:43 +0000

    fixed css formatting for analysis page

--- run_all.sh
+++ run_all.sh
@@ -4,4 +4,4 @@
 
 nohup /home/danielorf/commute_tracker/venv/bin/python /home/danielorf/commute_tracker/data_collection.py &
 cd /home/danielorf/commute_tracker
-sudo /home/danielorf/commute_tracker/venv/bin/gunicorn --workers=4 --bind=0.0.0.0:80 ct_server:app &
+sudo nohup /home/danielorf/commute_tracker/venv/bin/gunicorn --workers=4 --bind=0.0.0.0:80 ct_server:app &
